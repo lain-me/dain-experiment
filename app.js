4fa608c20993c673c54a19af5629d23eeae2721b
Andrey Lukyanenko <laser.me@yandex.ru>
2017-11-04 01:24:59 +0900

    Configs

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 var express = require('express');
-var app = express();
 const MongoClient = require('mongodb').MongoClient;
 
+var app = express();
 const url = 'mongodb://mongodb/';
 
 process.env.USER = process.env.USER || "";
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 	MongoClient.connect(url, function (err, db) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Connected correctly to server");
-            db.close();
-        }
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("Connected correctly to server");
+			db.close();
+		}
 
-    });
-  res.send("Hello World from " + process.env.USER);
+	});
+	res.send("Hello World from " + process.env.USER);
 });
 
-app.listen(3000, function(){
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
 });
