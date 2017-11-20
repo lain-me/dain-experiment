cc9eb0cc5a82e8a7fb0aed6c6dfc091caf3d6e87
Andrey Lukyanenko <laser.me@yandex.ru>
2017-11-20 16:03:47 +0900

    remove dist

--- dist/stack/abstract-packet.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var AbstractPacket = /** @class */ (function () {
-    function AbstractPacket() {
-    }
-    return AbstractPacket;
-}());
-exports.AbstractPacket = AbstractPacket;
